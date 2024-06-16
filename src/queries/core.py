775dd2de083d48ebb3006df178f89f0af5cd42c3
mrkoc <mr.kocta@bk.ru>
2024-06-16 23:34:02 +0300

    work with queries in sqlalch

---
+++ src/queries/core.py
@@ -0,0 +1,38 @@
+from sqlalchemy import text, insert
+from src.database import sync_engine, async_engine
+from src.models import metadata_obj, workers_table
+
+
+def get_123_sync():
+    with sync_engine.connect() as conn:
+        res = conn.execute(text("SELECT VERSION()"))
+        print(f"{res.first()=}")
+
+
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT VERSION()"))
+        print(f"{res.first()=}")
+
+
+def create_tables():
+    sync_engine.echo = False
+    metadata_obj.drop_all(sync_engine)
+    metadata_obj.create_all(sync_engine)
+    sync_engine.echo = True
+
+
+def insert_data():
+    with sync_engine.connect() as conn:
+        # stmt = """INSERT INTO workers (username) VALUES
+        # ('AO Bobr'),
+        # ('OOO Volk');
+        # """
+        stmt = insert(workers_table).values(
+            [
+                {'username': 'Volk'},
+                {'username': 'Bobr'}
+            ]
+        )
+        conn.execute(stmt)
+        conn.commit()
