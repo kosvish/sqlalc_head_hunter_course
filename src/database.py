58ab7d88b4f8f8bcd35a64e551e19aad764540eb
mrkoc <mr.kocta@bk.ru>
2024-06-16 23:34:29 +0300

    create_session, and Base class

--- src/database.py
+++ src/database.py
@@ -1,36 +1,34 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy import URL, create_engine, text
+from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
+from sqlalchemy import URL, create_engine, text, String
 from config import *
 import asyncio
-
+from typing import Annotated
 
 DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DATABASE_URL_ASYNC = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 sync_engine = create_engine(
     url=DATABASE_URL,
-    echo=False,
+    echo=True,
     pool_size=5,
     max_overflow=10
 )
 
 async_engine = create_async_engine(
     url=DATABASE_URL_ASYNC,
-    echo=False,
+    echo=True,
     pool_size=5,
     max_overflow=10
 )
 
-with sync_engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f"{res.first()=}")
+session_factory = sessionmaker(sync_engine)
+async_session_factory = async_sessionmaker(async_engine)
+
+str_200 = Annotated[str, 200]
 
 
-async def get_123():
-    async with async_engine.connect() as conn:
-        res = await conn.execute(text("SELECT VERSION()"))
-        print(f"{res.first()=}")
-
-asyncio.run(get_123())
-
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        str_200: String(200)
+    }
